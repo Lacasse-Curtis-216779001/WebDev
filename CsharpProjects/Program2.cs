160724f6992b382f1fc248e48bc3542965cbc42e
Lacasse-Curtis-216779001 <curtislacasse@gmail.com>
2025-02-04 14:42:24 -0500

    op

--- CsharpProjects/Program2.cs
+++ CsharpProjects/Program2.cs
@@ -2,3 +2,14 @@ int first = 2;
 string second = "4";
 string result = first + second;
 Console.WriteLine(result);
+
+string value = "102";
+int result = 0;
+
+if(int.TryParse(value, out result)){
+    Console.WriteLine($"Measurement: {result}");
+}
+else{
+    Console.WriteLine("Unaple to report the measurement.");
+}
+Console.WriteLine($"Measurement wit offset: fweh {50 + result}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c161ba5ad10beb7e2ad4a8f0861fdf82e88e9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77877b6c7d7acab8b11b953ded064e4f37b9e59b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
