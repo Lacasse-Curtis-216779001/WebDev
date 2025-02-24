32ed34e898bc222fe5de18ca80dc5674bc842a07
Lacasse-Curtis-216779001 <curtislacasse@gmail.com>
2025-02-24 16:52:32 -0500

    doing basic methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -321,5 +321,64 @@ Console.WriteLine($"     Sub Total: {subtotal:C}");
 Console.WriteLine($"           Tax: {taxPercentage:P2}");
 Console.WriteLine($"     Total Billed: {total:C}");
 
+
+
+string input = "Pad this";
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12,'-'));
+Console.WriteLine(input.PadRight(12,'-'));
+
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,00.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+comparisonMessage += currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}",currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}",newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
 */
 
+
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers(){
+Random random = new Random();
+for(int i = 0; i < 5; i++){
+    Console.Write($"{random.Next(1, 100)} ");
+}
+Console.WriteLine();
+}
