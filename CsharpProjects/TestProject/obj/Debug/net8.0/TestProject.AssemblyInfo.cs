eccd7a9431554417d4fef926ca4a56f4d0de9c5b
Lacasse-Curtis-216779001 <curtislacasse@gmail.com>
2025-03-04 12:51:42 -0500

    the random animals method

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357a1230d67f03779c8010f93095ca4152ba5478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51205673db6ee71be495ef1ce116c451ed9d8338")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
